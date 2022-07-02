ac4a4d97f77b3dd95760bb706d1cc0aaa083531b
moseskim <creatinov.kim@gmail.com>
2022-07-02 12:28:10 +0900

    updated qdata/practice

--- qdata/practice/notrap.bash
+++ qdata/practice/notrap.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $$      #自身のPIDを表示
-sleep 10000  #このスクリプトを終わらせないためにsleep
+echo $$      # 자신의 PID를 표시
+sleep 10000  # 이 스크립트를 종료하지 않도록 하기 위해 sleep
--- qdata/practice/trap.bash
+++ qdata/practice/trap.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-trap '' 2    #trapをしかける
+trap '' 2    # trap을 건다
 echo $$
 sleep 10000
