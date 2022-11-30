9c13b043b762021e8f4389b779e0620dfb37f9ee
srorien <59643714+srorien@users.noreply.github.com>
2022-12-01 08:15:28 +0900

    Update sample.lisp(qdata/135)
    일본어 설명을 한글로 변경

--- qdata/135/sample.lisp
+++ qdata/135/sample.lisp
@@ -1,13 +1,13 @@
-; n!(階乗)を返す
+; n!(팩토리얼)을 반환
 (defun fact (n) (if (<= n 1) 1 (* n (fact (- n 1)))))
 
-; n番目のフィボナッチ数を返す
+; n번째 피보나치 수를 반환
 (defun fib (n) (if (<= n 1) n (+ (fib (- n 1) (fib (- n 2)))))
 
-; 1からnまでの総和を返す
+; 1부터 n까지의 총합을 반환
 (defun sum1 (n) (if (<= n 1) n (+ n (sum1 (- n 1))))
 
-;; 実行
+;; 실행
 (format t "fact:~D~%fib:~D~%sum1:~D"
   (fact 5)
   (fib 5)
